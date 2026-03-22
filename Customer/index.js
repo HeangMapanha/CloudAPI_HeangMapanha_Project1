ed6deb91da6b0a07580bf25e27ce336d1edd9cd7
Heang Mapanha <Mapanha.heang@gmail.com>
2026-03-22 09:46:19 +0700

    Update

---
+++ Customer/index.js
@@ -0,0 +1,96 @@
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const dbconnect = require('./dbconnect.js');
+const PersonModel = require('./schema/person_schema.js');
+const OrderModel = require('./schema/order_schema.js');
+const ProductModel = require('./schema/product_schema.js');
+
+
+app.get('/product', async (req, res) => {
+       try {
+          const products = await ProductModel.find(); // No filter = get all
+          res.status(200).json(products);
+  } 
+     catch (err) {
+          res.status(500).json({ message: err.message });
+  }
+})
+app.get('/product/:product_name', async (req, res) => {
+  try {
+    const product = await ProductModel.findOne({ "product_name": req.params.product_name });
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+      return res.status(200).json(product);
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post('/checkout', async (req, res) => {
+  profile_user = await PersonModel.findOne({"emailid": req.body.emailid})
+  product_info = await ProductModel.findOne({"product_name": req.body.product_name})
+  const pobj = OrderModel({
+    user_name: profile_user.user_name,
+    user_emailid: req.body.emailid,
+    product_name: req.body.product_name,
+    status : "in-processing"
+  })
+  if (profile_user != null && product_info != null) {
+    pobj.save()
+    .then(insertdocuemnt => {
+      console.log("Product saved")
+      console.log(insertdocuemnt)
+      res.status(200).json(insertdocuemnt);
+    })
+    .catch(err => {
+        res.status(500).send({message: err.message})
+    });
+  }
+})
+
+app.get('/yourorder/:emailid', async (req, res) => {
+ try {
+    const order = await OrderModel.find({ "user_emailid": req.params.emailid });
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    res.status(200).json(order);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+
+}) // customers view thier orders
+
+app.delete('/yourorder/:emailid', async (req, res) => {
+ try {
+  const delete_order = await OrderModel.findOneAndDelete({
+    "user_emailid": req.params.emailid,
+    "product_name": req.body.product_name,
+    "status": "in-processing"})
+    if (delete_order) {
+      console.log("Order Canceled Successfully")
+      res.status(500).json("Order Canceled Successfully");
+    }
+    else {
+      console.log("Error canceling order, order may already been processed.")
+      res.status(200).json("Error canceling order, order may already been processed.");
+      }
+    }
+ catch (err) {
+          res.status(500).json({ message: err.message });
+  }
+
+}) // customer delete thier order that haven't been process yet
+
+
+
+
+app.listen(5003, () =>
+    console.log('EXPRESS Server Started at Port No: 5003'));
